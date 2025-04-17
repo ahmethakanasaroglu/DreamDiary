3098b06b5027361ca40a87de3a44bf36e1f2fb5d
asarogluahmethakan <asarogluahmethakan@gmail.com>
2025-04-17 16:51:59 +0300

    APIKeys Güncellendi, geminiAIAPI silindi

--- DreamDiary/Utils/APIKeys.swift
+++ DreamDiary/Utils/APIKeys.swift
@@ -2,6 +2,5 @@ import Foundation
 
 struct APIKeys {
     // MARK: - Gerçek API anahtarınızı buraya ekleyin
-    static let geminiAI = "AIzaSyDj-ua-9osvlv6A6HPKy3bVgEHMLRdBi8M"
     static let huggingFace = "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"
 }

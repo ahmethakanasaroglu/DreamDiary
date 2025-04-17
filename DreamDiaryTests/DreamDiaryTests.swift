82af53f28f4404b036bc6700b99efe583ac12d31
asarogluahmethakan <asarogluahmethakan@gmail.com>
2025-04-17 16:10:26 +0300

    Initial Commit

---
+++ DreamDiaryTests/DreamDiaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  DreamDiaryTests.swift
+//  DreamDiaryTests
+//
+//  Created by Ahmet Hakan Asaroğlu on 17.04.2025.
+//
+
+import Testing
+@testable import DreamDiary
+
+struct DreamDiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

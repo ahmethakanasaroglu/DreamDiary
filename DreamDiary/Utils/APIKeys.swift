e2bc60f561a999711f49d9f70842608636aa7fcc
asarogluahmethakan <asarogluahmethakan@gmail.com>
2025-04-17 17:08:16 +0300

    APIKeys'e geminiAIAPIKEY eklendi. SearchView'da tag'lerden arama yaparken lazım

--- DreamDiary/Utils/APIKeys.swift
+++ DreamDiary/Utils/APIKeys.swift
@@ -2,5 +2,6 @@ import Foundation
 
 struct APIKeys {
     // MARK: - Gerçek API anahtarınızı buraya ekleyin
+    static let geminiAI = "AIzaSyDj-ua-9osvlv6A6HPKy3bVgEHMLRdBi8M"
     static let huggingFace = "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"
 }

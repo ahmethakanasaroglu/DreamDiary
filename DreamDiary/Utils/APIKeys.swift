e219160725d8d75d7f9297cf553334a6ff875b39
asarogluahmethakan <asarogluahmethakan@gmail.com>
2025-04-20 23:28:06 +0300

    HugginFace token silindi

--- DreamDiary/Utils/APIKeys.swift
+++ DreamDiary/Utils/APIKeys.swift
@@ -3,6 +3,5 @@ import Foundation
 struct APIKeys {
     // MARK: - Gerçek API anahtarınızı buraya ekleyin
     static let geminiAI = "AIzaSyDj-ua-9osvlv6A6HPKy3bVgEHMLRdBi8M"
-    //static let huggingFace = "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"
     static let openAI = "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"
 }

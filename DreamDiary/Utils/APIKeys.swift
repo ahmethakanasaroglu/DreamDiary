02865e37b5a695436b5a725ae0177292b5eec875
asarogluahmethakan <asarogluahmethakan@gmail.com>
2025-04-21 09:57:29 +0300

    APIKeys.swift dosyası takipten çıkarıldı

--- DreamDiary/Utils/APIKeys.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct APIKeys {
-    // MARK: - Gerçek API anahtarınızı buraya ekleyin
-    static let geminiAI = "AIzaSyDj-ua-9osvlv6A6HPKy3bVgEHMLRdBi8M"
-    static let openAI = "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"
-}
